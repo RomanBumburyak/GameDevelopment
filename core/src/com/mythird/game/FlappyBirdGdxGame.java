cf3e9f16523f76b2f501e8f7469715ecd1f3312a
RomanBumburyak <romanbumburyak@gmail.com>
2018-10-08 21:07:44 -0400

    Added several new variables with different types

--- core/src/com/mythird/game/FlappyBirdGdxGame.java
+++ core/src/com/mythird/game/FlappyBirdGdxGame.java
@@ -12,11 +12,18 @@ public class FlappyBirdGdxGame extends ApplicationAdapter {
 
 	Texture[] birds;
 	int flapState = 0;
+	float birdY = Gdx.graphics.getHeight() / 2 - birds[0].getHeight() / 2;
+	float velocity = 0;
+
+	int gameState = 0;
+
+
+
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		background = new Texture("bg.png");
+		background = new Texture("blueBack.jpg");
 		birds = new Texture[2];
 		birds[0] = new Texture("birdUpFlap.png");
 		birds[1] = new Texture("birdDownFlap.png");
@@ -28,18 +35,38 @@ public class FlappyBirdGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 
-		if( flapState == 0) {
-			flapState =1;
+		if (Gdx.input.justTouched()){
+			Gdx.app.log("Touched", "yep");
+			gameState = 1;
+
+		}
+
+		if (gameState != 0) {
+
+			velocity++;
+			birdY -= velocity;
+
+
+		} else { //only doing this check if the game state is zero.
+
+			  if (Gdx.input.justTouched()) {
+				  gameState = 1;
+			  }
+		}
+
+		if (flapState == 0) {
+			flapState = 1;
 		} else {
 			flapState = 0;
 		}
 
 		batch.begin();
-		batch.draw(background, 0, 0 , Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(birds[flapState], Gdx.graphics.getWidth()/ 2 - birds[flapState].getWidth() / 2, Gdx.graphics.getHeight()/2 - birds[flapState].getHeight() / 2);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
 		batch.end();
 
 	}
 
-
 }
+
+
